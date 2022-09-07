d3a5b3c7b5449dd57128f80170013b99470b27d1
sakthivel9963 <sakthivel9963@gmail.com>
2022-09-07 11:16:54 +0530

    :tada: :sparkles: sublime install code change

--- install/install.sh
+++ install/install.sh
@@ -172,7 +172,7 @@ echo "
 Sublime text
 ***********************************************
 ";
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
